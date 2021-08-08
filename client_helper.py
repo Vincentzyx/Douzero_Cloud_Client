2a13e240c57f640bf1952e513734c8647a9e864c
Vincentzyx <929403983@qq.com>
2021-08-08 15:21:43 +0800

    Update client_helper.py

--- client_helper.py
+++ client_helper.py
@@ -6,12 +6,9 @@ import requests
 import time
 import json
 import hashlib
-import traceback
+import gzip
 debug = False
-google_cloud = True
 HOST = "http://mc.vcccz.com:15000"
-if google_cloud:
-    HOST = "http://cf.vcccz.com/"
 if debug:
     HOST = "http://127.0.0.1:5000"
 
@@ -53,7 +50,8 @@ start_time = time.time()
 def handle_batch(position, batch, model_version):
     try:
         data = pickle.dumps({"position" : position,  "batch": pack_batch(batch), "model_version": model_version})
-        rep = requests.post(HOST + "/upload_batch", data, headers={'Content-Type': 'application/octet-stream'})
+        data = gzip.compress(data)
+        rep = requests.post(HOST + "/upload_batch", data, headers={'Content-Type': 'application/octet-stream', 'Content-Encoding':  'gzip'})
         ret = rep.json()
         if "server_speed" in ret:
             print("上传成功，服务器当前速度: %.1f fps" % ret["server_speed"])
@@ -65,51 +63,30 @@ def handle_batch(position, batch, model_version):
 
 def handle_batches(batches, model_version):
     data = []
-
-    for batch in batches:
-        data.append({
-            "position": batch["position"],
-            "batch": pack_batch(batch["batch"]),
-        })
-    info = {
-        "batches": data,
-        "model_version": model_version
-    }
-    # print(batches)
-    data = pickle.dumps(info)
-    tryCount = 2
-    rep = None
-    print("准备发送Batch")
     try:
+        for batch in batches:
+            data.append({
+                "position": batch["position"],
+                "batch": pack_batch(batch["batch"]),
+            })
+        info = {
+            "batches": data,
+            "model_version": model_version
+        }
+        # print(batches)
+        data = pickle.dumps(info)
+        rep = requests.post(HOST + "/upload_batch", data, headers={'Content-Type': 'application/octet-stream'})
         try:
-            rep = requests.post(HOST + "/upload_batch", data, headers={'Content-Type': 'application/octet-stream'}, timeout=60)
-        except TimeoutError:
-            rep = None
-            print("传输超时")
-        while (rep is None or rep.status_code != 200) and tryCount > 0:
-            tryCount -= 1
-            print("传输失败，重试中")
-            try:
-                rep = requests.post(HOST + "/upload_batch", data, headers={'Content-Type': 'application/octet-stream'}, timeout=60)
-            except TimeoutError:
-                rep = None
-                print("传输超时")
-        if rep is not None and rep.status_code == 200:
-            print("传输成功")
-            try:
-                ret = rep.json()
-                print(ret)
-                if "server_speed" in ret:
-                    print("上传成功，服务器当前速度: %.1f fps" % ret["server_speed"])
-                if "model_version" in ret:
-                    return ret["model_version"], ret["model_url"]
-                else:
-                    return -1, ""
-            except json.JSONDecodeError:
-                print("Json Decode Error")
-    except Exception as e:
-        print("Batch传送失败:", repr(e))
-
+            ret = rep.json()
+            print(ret)
+            if "server_speed" in ret:
+                print("上传成功，服务器当前速度: %.1f fps" % ret["server_speed"])
+            if "model_version" in ret:
+                return ret["model_version"], ret["model_url"]
+        except json.JSONDecodeError:
+            print("Json Decode Error")
+    except Exception:
+        print("Batch 传送失败")
     return model_version, ""
 
 def get_model_info():
