8b28c1e8b78efd834f91f45f71323cbdcaf784c3
Vincentzyx <929403983@qq.com>
2021-08-18 11:21:17 +0800

    Update dmc.py

--- douzero/dmc/dmc.py
+++ douzero/dmc/dmc.py
@@ -312,7 +312,7 @@ def train(flags):
             start_frames = frames
             position_start_frames = {k: position_frames[k] for k in position_frames}
             start_time = timer()
-            time.sleep(5)
+            time.sleep(10)
 
             if timer() - last_checkpoint_time > flags.save_interval * 60:
                 checkpoint(frames)
