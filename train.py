839295fde2583f9ebe55ea85677a8d9725f5170d
Vincentzyx <929403983@qq.com>
2021-08-27 02:49:03 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,5 +5,4 @@ from douzero.dmc import parser, train
 if __name__ == '__main__':
     flags = parser.parse_args()
     os.environ["CUDA_VISIBLE_DEVICES"] = flags.gpu_devices
-    flags.objective = "wp"
     train(flags)
