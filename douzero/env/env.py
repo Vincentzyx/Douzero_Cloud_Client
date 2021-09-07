99831a028c1f4df5e3339d1a1e3aa6f40a393fb7
Vincentzyx <929403983@qq.com>
2021-09-07 15:35:39 +0800

    Fix step reward

--- douzero/env/env.py
+++ douzero/env/env.py
@@ -269,14 +269,14 @@ class Env:
         self_bomb_num = self._env.pos_bomb_num[pos]
         if winner == 'landlord':
             if self.objective == 'adp':
-                return (1.1 - self._env.step_count * 0.0033) * 1.3 ** (bomb_num +self._env.multiply_count[pos]) /8
+                return (1.1 - self._env.step_count * 0.0033) * 1.3 ** (bomb_num +self._env.multiply_count[pos]) / 8
             elif self.objective == 'logadp':
                 return (1.0 - self._env.step_count * 0.0033) * 1.3**self_bomb_num * 2**self._env.multiply_count[pos] / 4
             else:
                 return 1.0 - self._env.step_count * 0.0033
         else:
             if self.objective == 'adp':
-                return (-1.1 - self._env.step_count * 0.0033) * 1.3 ** (bomb_num +self._env.multiply_count[pos]) /8
+                return (-1.1 + self._env.step_count * 0.0033) * 1.3 ** (bomb_num +self._env.multiply_count[pos]) / 8
             elif self.objective == 'logadp':
                 return (-1.0 + self._env.step_count * 0.0033) * 1.3**self_bomb_num * 2**self._env.multiply_count[pos] / 4
             else:
