c9d09c5cdffeb395f3017219e88269ca78946ebc
Vincentzyx <929403983@qq.com>
2021-09-07 17:05:14 +0800

    Update env.py

--- douzero/env/env.py
+++ douzero/env/env.py
@@ -6,7 +6,7 @@ import BidModel
 
 from douzero.env.game import GameEnv
 
-env_version = "3.0.0"
+env_version = "3.0.1"
 env_url = "http://od.vcccz.com/hechuan/env.py"
 Card2Column = {3: 0, 4: 1, 5: 2, 6: 3, 7: 4, 8: 5, 9: 6, 10: 7,
                11: 8, 12: 9, 13: 10, 14: 11, 17: 12}
