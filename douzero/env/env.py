f8ac5c08d1ec6703f2bdb853b2068cb953eae169
Vincentzyx <929403983@qq.com>
2021-09-12 09:27:19 +0800

    Update env.py

--- douzero/env/env.py
+++ douzero/env/env.py
@@ -6,7 +6,7 @@ import BidModel
 
 from douzero.env.game import GameEnv
 
-env_version = "3.1.0"
+env_version = "3.1.1"
 env_url = "http://od.vcccz.com/hechuan/env.py"
 Card2Column = {3: 0, 4: 1, 5: 2, 6: 3, 7: 4, 8: 5, 9: 6, 10: 7,
                11: 8, 12: 9, 13: 10, 14: 11, 17: 12}
@@ -269,20 +269,36 @@ class Env:
         winner = self._game_winner
         bomb_num = self._game_bomb_num
         self_bomb_num = self._env.pos_bomb_num[pos]
-        if winner == 'landlord':
-            if self.objective == 'adp':
-                return (1.0 * 1.3 ** (bomb_num + self._env.multiply_count[pos]) - self._env.step_count * 0.001) / 8
-            elif self.objective == 'logadp':
-                return (1.0 - self._env.step_count * 0.0033) * 1.3**self_bomb_num * 2**self._env.multiply_count[pos] / 4
+        if pos == "landlord":
+            if winner == 'landlord':
+                if self.objective == 'adp':
+                    return (1.1 - self._env.step_count * 0.0033) * 1.3 ** (bomb_num + self._env.multiply_count[pos]) / 8
+                elif self.objective == 'logadp':
+                    return (1.0 - self._env.step_count * 0.0033) * 1.3**self_bomb_num * 2**self._env.multiply_count[pos] / 4
+                else:
+                    return 1.0 - self._env.step_count * 0.0033
             else:
-                return 1.0 - self._env.step_count * 0.0033
+                if self.objective == 'adp':
+                    return (-1.1 - self._env.step_count * 0.0033) * 1.3 ** (bomb_num + self._env.multiply_count[pos]) / 8
+                elif self.objective == 'logadp':
+                    return (-1.0 + self._env.step_count * 0.0033) * 1.3**self_bomb_num * 2**self._env.multiply_count[pos] / 4
+                else:
+                    return -1.0 + self._env.step_count * 0.0033
         else:
-            if self.objective == 'adp':
-                return (-1.0 * 1.3 ** (bomb_num + self._env.multiply_count[pos]) + self._env.step_count * 0.001) / 8
-            elif self.objective == 'logadp':
-                return (-1.0 + self._env.step_count * 0.0033) * 1.3**self_bomb_num * 2**self._env.multiply_count[pos] / 4
+            if winner == 'landlord':
+                if self.objective == 'adp':
+                    return (1.0 * 1.3 ** (bomb_num + self._env.multiply_count[pos]) - self._env.step_count * 0.001) / 8
+                elif self.objective == 'logadp':
+                    return (1.0 - self._env.step_count * 0.0033) * 1.3**self_bomb_num * 2**self._env.multiply_count[pos] / 4
+                else:
+                    return 1.0 - self._env.step_count * 0.0033
             else:
-                return -1.0 + self._env.step_count * 0.0033
+                if self.objective == 'adp':
+                    return (-1.0 * 1.3 ** (bomb_num + self._env.multiply_count[pos]) + self._env.step_count * 0.001) / 8
+                elif self.objective == 'logadp':
+                    return (-1.0 + self._env.step_count * 0.0033) * 1.3**self_bomb_num * 2**self._env.multiply_count[pos] / 4
+                else:
+                    return -1.0 + self._env.step_count * 0.0033
 
     def _get_reward_bidding(self, pos):
         """
